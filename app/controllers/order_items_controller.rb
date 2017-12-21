362628d2ae10f96c8da80c76388443a24f9ed70a
Rik Lomas <lomas.rik@gmail.com>
2017-12-21 18:26:25 -0500

    Fix

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -4,11 +4,9 @@ class OrderItemsController < ApplicationController
     @product = Product.find(params[:product_id])
 
     @quantity = form_params[:quantity]
-
-    @size = form_params[:product_variant_id]
-
-
-    @current_cart.order_items.create(product: @product, quantity: @quantity, product_variant_id: @size )
+    @variant = form_params[:product_variant_id]
+    
+    @current_cart.order_items.create(product: @product, quantity: @quantity, product_variant_id: @variant)
 
     flash[:success] = "Item added to cart"
 
