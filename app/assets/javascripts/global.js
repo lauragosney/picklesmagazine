cebaa084b329cd157c6685871fe4bddc20e697fc
Laura Gosney <lauragosney82@gmail.com>
2018-01-08 20:26:03 +0000

    front end style changes added

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,4 +1,4 @@
-$(document).on("ready page:load", function () {
+$(document).on("ready turbolinks:load", function () {
 
     $('.delivery_address').hide();
 
@@ -10,4 +10,70 @@ $(document).on("ready page:load", function () {
          $('.delivery_address').show(300);
        }
     });
+
+    $(document).on("scroll", function () {
+
+
+      var pageTop = $(document).scrollTop()
+      var pageBottom = pageTop + $(window).height()
+      var tags = $("section")
+
+      for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i]
+
+        if ($(tag).position().top < pageBottom) {
+          $(tag).addClass("visible")
+
+        } else {
+          $(tag).removeClass("visible")
+        }
+
+      if (pageTop > 200) {
+          $(".hero").addClass("fade")
+      } else
+      	$(".hero").removeClass("fade")
+      }
+    })
+
+    $('div.menu-toggle').on('click',function(){
+      $('.menu').toggleClass('open')
+
+    })
+    $("div.menu-toggle").on("click", function () {
+
+      if ($("img.icon").attr("src") == "menu-icon.svg") {
+         $("img.icon").attr("src", "menu-close.svg")
+      } else {
+         $("img.icon").attr("src", "menu-icon.svg")
+      }
+    })
+    $(".homehover").hover(function () {
+      $("div.navigation-overlay-bg-home-hover").fadeIn()
+    }, function () {
+      $("div.navigation-overlay-bg-home-hover").fadeOut()
+    })
+    $(".shophover").hover(function () {
+      $("div.navigation-overlay-bg-shop-hover").fadeIn()
+    }, function () {
+      $("div.navigation-overlay-bg-shop-hover").fadeOut()
+    })
+    $(".storieshover").hover(function () {
+      $("div.navigation-overlay-bg-stories-hover").fadeIn()
+    }, function () {
+      $("div.navigation-overlay-bg-stories-hover").fadeOut()
+    })
+    $(".abouthover").hover(function () {
+      $("div.navigation-overlay-bg-about-hover").fadeIn()
+    }, function () {
+      $("div.navigation-overlay-bg-about-hover").fadeOut()
+    })
+    $(".contacthover").hover(function () {
+      $("div.navigation-overlay-bg-contact-hover").fadeIn()
+    }, function () {
+      $("div.navigation-overlay-bg-contact-hover").fadeOut()
+    })
+
+
+
+
 })
