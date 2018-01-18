4b7dd710f8ce5d5c04bef820aea124d7d0e27ca0
Laura Gosney <lauragosney82@gmail.com>
2018-01-18 23:57:50 +0000

    newsletter fields added

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -2,8 +2,8 @@ class NewslettersController < ApplicationController
 
   def create
     gibbon = Gibbon::Request.new(api_key: "a18090f4e9faace25281903f9f60350f-us11")
-    gibbon.lists("a52bb1c17c").members.create(body: { email_address: params["newsletter"]["email"], status: "subscribed" })
-  
+    gibbon.lists("a52bb1c17c").members.create(body: { email_address: params["newsletter"]["email"]["first_name"]["last_name"], status: "subscribed" })
+
         flash[:success] = "Product removed from cart"
 
         redirect_to newsletter_path
