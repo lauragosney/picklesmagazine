8e6759e5dd37ddb810c68f109f5499718da37af7
Laura Gosney <lauragosney82@gmail.com>
2018-01-19 00:20:08 +0000

    newsletter redirect_to added

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -2,11 +2,13 @@ class NewslettersController < ApplicationController
 
   def create
     gibbon = Gibbon::Request.new(api_key: "a18090f4e9faace25281903f9f60350f-us11")
-    gibbon.lists("a52bb1c17c").members.create(body: { email_address: params["newsletter"]["email"]["first_name"]["last_name"], status: "subscribed" })
+    gibbon.lists("a52bb1c17c").members.create(body: { email_address: params["newsletter"]["email"], first_name: params["newsletter"]["first_name"], last_name: params["newsletter"]["last_name"], status: "subscribed" })
 
-        flash[:success] = "Product removed from cart"
+      flash[:success] = "You have successfully subscribed to Pickles Magazine "
 
-        redirect_to newsletter_path
+      redirect_to newsletter_path
   end
+
+
 
 end
