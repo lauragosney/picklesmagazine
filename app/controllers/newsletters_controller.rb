491f7fa7b49f9f9a428b8df84811d312c5862aef
Laura Gosney <lauragosney82@gmail.com>
2018-01-19 00:51:26 +0000

    amended secrets

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,7 +1,7 @@
 class NewslettersController < ApplicationController
 
   def create
-    gibbon = Gibbon::Request.new(api_key: "a18090f4e9faace25281903f9f60350f-us11")
+    gibbon = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key )
     gibbon.lists("a52bb1c17c").members.create(body: { email_address: params["newsletter"]["email"], first_name: params["newsletter"]["first_name"], last_name: params["newsletter"]["last_name"], status: "subscribed" })
 
       flash[:success] = "You have successfully subscribed to Pickles Magazine "
