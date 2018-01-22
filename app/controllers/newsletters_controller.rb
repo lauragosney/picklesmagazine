a7740090a942ec46123ee45d460d5431d773bf83
Rik Lomas <lomas.rik@gmail.com>
2018-01-22 15:39:50 -0500

    md5 email

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -2,13 +2,22 @@ class NewslettersController < ApplicationController
 
   def create
     gibbon = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key )
-    gibbon.lists("a52bb1c17c").members.create(body: { email_address: params["newsletter"]["email"], first_name: params["newsletter"]["first_name"], last_name: params["newsletter"]["last_name"], status: "subscribed" })
+    
+    email = params["newsletter"]["email"]
+    md5_email = Digest::MD5.hexdigest(email.downcase)
+    
+    gibbon.lists("a52bb1c17c").members(md5_email).upsert(body: { 
+      email_address: email, 
+      status: "subscribed",
+      merge_fields: {
+        FNAME: params["newsletter"]["first_name"],
+        LNAME: params["newsletter"]["last_name"]
+      }
+    })
 
-      flash[:success] = "You have successfully subscribed to Pickles Magazine "
+    flash[:success] = "You have successfully subscribed to Pickles Magazine "
 
-      redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path)
   end
-
-
 
 end
