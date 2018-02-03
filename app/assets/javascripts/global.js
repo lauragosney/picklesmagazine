f414bebbb4be8990300c927ad4afd5ec48f035f9
Laura Gosney <lauragosney82@gmail.com>
2018-02-03 21:09:48 +0000

    styling product show page

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -11,7 +11,7 @@ $(document).on("turbolinks:load", function (ev) {
      }
   });
 
-  $(document).on("scroll", function () {    
+  $(document).on("scroll", function () {
     var pageTop = $(document).scrollTop()
     var pageBottom = pageTop + $(window).height()
     var tags = $("section")
