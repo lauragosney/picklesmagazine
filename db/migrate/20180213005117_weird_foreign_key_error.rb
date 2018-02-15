dd46619dce56e545a9c8f284f99d8575ecc53d80
Rik Lomas <lomas.rik@gmail.com>
2018-02-15 17:27:52 -0500

    comment this out for heroku

--- db/migrate/20180213005117_weird_foreign_key_error.rb
+++ db/migrate/20180213005117_weird_foreign_key_error.rb
@@ -1,7 +1,7 @@
 class WeirdForeignKeyError < ActiveRecord::Migration[5.1]
   def change
-    add_foreign_key :products, :product_variants, on_delete: :cascade
-    add_foreign_key :product_variants, :products, on_delete: :cascade
-    add_index :product_variants, :product_id
+    # add_foreign_key :products, :product_variants, on_delete: :cascade
+    # add_foreign_key :product_variants, :products, on_delete: :cascade
+    # add_index :product_variants, :product_id
   end
 end
