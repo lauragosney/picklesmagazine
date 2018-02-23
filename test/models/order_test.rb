8ddbfca8ea93930d5ac209159213c7681af5e148
Laura Gosney <lauragosney82@gmail.com>
2018-02-23 22:58:13 +0000

    homepage cta links

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save is order is full" do
+    @order = orders(:laura)
+
+    assert_equal @order.save, true
+
+  end
+
+  test "should save and charge" do
+    @order = orders(:laura)
+
+    assert_equal @order.save_and_charge, true
+
+  end
+
+  test "should not save and charge with invalid card" do
+    @order = orders(:ed)
+
+    assert_equal @order.save_and_charge, false
+    assert_equal @order.errors[:stripe_token], ["Your card was declined."]
+
+  end
 end
