857c6f7eb702b7b20220fdc4a7f0088b9e6967d0
Laura Gosney <lauragosney82@gmail.com>
2018-02-28 22:42:47 +0000

    added privacy and terms

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
 
   get "privacypolicy", to: "pages#privacypolicy"
 
+  get "termsandconditions", to: "pages#termsandconditions"
+
   get "about", to: "pages#about"
 
   get "contact", to: "pages#contact"
