73322808006e89c33fd1c676e61c1d0568f21cd7
Laura Gosney <lauragosney82@gmail.com>
2018-03-02 11:49:51 +0000

    added meta for sharing

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -22,4 +22,29 @@ class OrderTest < ActiveSupport::TestCase
     assert_equal @order.errors[:stripe_token], ["Your card was declined."]
 
   end
+
+  test "should not save and charge with invalid expired card" do
+    @order = orders(:jo)
+
+    assert_equal @order.save_and_charge, false
+    assert_equal @order.errors[:stripe_token], ["Your card's security code is incorrect."]
+
+  end
+
+  test "should not save and charge with processing error" do
+    @order = orders(:gabs)
+
+    assert_equal @order.save_and_charge, false
+    assert_equal @order.errors[:stripe_token], ["An error occurred while processing your card. Try again in a little bit."]
+
+  end
+
+  test "should not save and charge as card is considered fraudulent" do
+    @order = orders(:dan)
+
+    assert_equal @order.save_and_charge, false
+    assert_equal @order.errors[:stripe_token], ["Your card was declined."]
+
+  end
+
 end
