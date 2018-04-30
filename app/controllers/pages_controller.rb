f5cdc36c1cce7fd5263feb7bb7e7a8ead9a97541
Laura Gosney <lauragosney82@gmail.com>
2018-04-30 12:12:47 +0100

    change order of stories on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   def home
     @featured_products = Product.where(is_featured: true)
-    @featured_stories = Story.where(is_featured: true)
+    @featured_stories = Story.where(is_featured: true).order('created_at DESC')
 
     @product_launch1 = Product.where(on_homepage: true).last
     @product_launch2 = Product.where(on_homepage_slottwo: true).last
