4aebd6ae30a8239c57fca9cd7fb80b2d8fcdb55d
Rik Lomas <lomas.rik@gmail.com>
2018-05-17 17:35:19 -0400

    create cart if not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,20 +5,14 @@ class ApplicationController < ActionController::Base
   helper_method :current_cart
 
   def current_cart
-
     if session[:cart_id].present?
       @current_cart = Cart.find(session[:cart_id])
     else
       @current_cart = Cart.create
       session[:cart_id] = @current_cart.id
+    end
+  rescue ActiveRecord::RecordNotFound
+    @current_cart = Cart.create
+    session[:cart_id] = @current_cart.id
   end
-
-
- end
-
-
-
-
-
-
 end
