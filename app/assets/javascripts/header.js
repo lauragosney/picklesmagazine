acad04ae6ec4df3d314c77138577332592a719a2
Laura Gosney <lauragosney82@gmail.com>
2019-07-15 20:51:37 +0100

    header tweaks

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,16 +1,13 @@
 $(document).on("ready", function () {
 
   const headerTag = document.querySelector('header')
-  const headerBlack = document.querySelector('.header_black_hide')
 
   const toggleHeader = function () {
     const pixels = window.pageYOffset
   		if(pixels > 60) {
        		headerTag.classList.add('scrolled')
-          headerBlack.classList.add('header_black_visible')
        } else {
          headerTag.classList.remove('scrolled')
-         headerBlack.classList.remove('header_black_visible')
        }
   }
 
