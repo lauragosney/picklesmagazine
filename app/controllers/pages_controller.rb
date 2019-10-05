8433473f8da99e967eb0af82d2d7d831d69362df
Laura Gosney <lauragosney82@gmail.com>
2019-10-05 21:58:02 +0100

    featured images order changed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def home
-    @featured_products = Product.where(is_featured: true)
+    @featured_products = Product.where(is_featured: true).order('created_at DESC')
     @featured_stories = Story.where(is_featured: true).order('created_at DESC')
 
     @product_launch1 = Product.where(on_homepage: true).last
