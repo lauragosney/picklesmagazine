5775df11fd33ae518c866af71570ea96ee3bd2e0
Laura Gosney <lauragosney82@gmail.com>
2020-05-26 22:11:02 +0100

    new season changes

---
+++ app/assets/javascripts/promo_banner.js
@@ -0,0 +1,23 @@
+// $(document).on("ready", function () {
+//   const promoBanner = document.querySelector('.slide-in-promo-banner')
+//   const toggleBanner = function () {
+//     const pixelsBanner = window.pageYOffset
+//     if(pixelsBanner > 800) {
+//       promoBanner.classList.add('show-banner')
+//      } else {
+//        promoBanner.classList.remove('show-banner')
+//      }
+//   }
+//
+//   toggleBanner()
+//
+//   document.addEventListener('scroll', function() {
+//     toggleBanner()
+//   })
+
+  // const closeBanner = document.querySelector('.close-banner')
+  //
+  // closeBanner.addEventListener('click', function() {
+  //    promoBanner.classList.remove('show-banner')
+  // })
+// })
